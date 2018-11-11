5eea1a9a55e5522bc19ad9c1656b2a5b3ff8a9fd
Carlos Azaustre <cazaustre@gmail.com>
2018-11-11 07:55:43 +0100

    events and styles on Media component

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+    console.log();
+  }
+
   render() {
     return (
-      <div className="Media">
-        <div>
+      <div className="Media" onClick={this.handleClick}>
+        <div className="Media-cover">
           <img 
-            src=""
+            src={this.props.image}
             alt=""
             width={260}
             height={160}
+            className="Media-image"
           />
-          <h3>Video Title</h3>
-          <p>Video Description</p>
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.props.author}</p>
         </div>
       </div>
     )
   }
 }
+
+Media.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  author: PropTypes.string,
+};
 
 export default Media;
